7c8364da06efe26bfc608c88f3534939ce5905fe
Alena <alena@Dmitrijs-MacBook-Air.local>
2024-04-16 17:54:55 +0200

    Add functions invertObject, trimSymbols, uniq

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,25 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    let result = '';
+    let count = 0;
+    let prevChar = '';
 
+    if(size === 0) return result;
+    if(size === undefined) return string;
+
+    for(const char of string) {
+        if(char === prevChar) {
+            count++;
+            if(count <= size) {
+                result += char;
+            }
+        } else {
+            count = 1;
+            prevChar = char;
+            result += char;
+        }
+        
+    }
+    return result;
 }

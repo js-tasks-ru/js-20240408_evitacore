f9b06cda0c9e8e3a9fd2b58de4834497ed91935a
Alena <alena@Dmitrijs-MacBook-Air.local>
2024-05-06 18:45:29 +0200

    Add component ColumnChartV2 7.1

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,7 +1,69 @@
 import fetchJson from './utils/fetch-json.js';
+import ColumnChartV1 from '../../04-oop-basic-intro-to-dom/1-column-chart/index.js';
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ColumnChart {
+export default class ColumnChartV2 extends ColumnChartV1 {
+    subElements = {};
 
+    constructor(props = {}) {
+        super(props);
+        this.url = new URL(props.url, BACKEND_URL);
+        this.from = props.range?.from ?? new Date(0);
+        this.to = props.range?.to ?? new Date(0);
+
+        this.createSubElements();
+        this.update(this.from, this.to);
+    }
+
+    createSubElements() {
+        const elements = this.element.querySelectorAll('[data-element]');
+        for (const element of elements) {
+            this.subElements[element.dataset.element] = element;
+        }
+        console
+        return this.subElements;
+    }
+     
+    async update(from, to) {
+        this.element.classList.add('column-chart_loading');
+        const data = await this.loadData(from, to);
+        super.update(Object.values(data));
+        this.element.classList.remove('column-chart_loading');
+        return data;
+    }
+
+    async loadData(from, to) {
+        this.url.searchParams.set('from', from);
+        this.url.searchParams.set('to', to);
+        const response = await fetchJson(this.url);
+        return response;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async render(from, to) {
+//     this.element.classList.add('column-chart_loading');
+//     const data = await this.getData(from, to);
+//     super.update(Object.values(data));
+//     this.element.classList.remove('column-chart_loading');
+//     return data;
+//   }
+
+//   async getData(from, to) {
+//     this.url.searchParams.set('from', from);
+//     this.url.searchParams.set('to', to);
+//     const response = await fetchJson(this.url);
+//     return response;
+//   }
